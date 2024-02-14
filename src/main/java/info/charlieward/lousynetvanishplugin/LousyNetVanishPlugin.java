6550c7dca0cf0abbbdd0bd19f8056c8597b4c4f1
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-14 14:35:37 +0000

    Initial

---
+++ src/main/java/info/charlieward/lousynetvanishplugin/LousyNetVanishPlugin.java
@@ -0,0 +1,17 @@
+package info.charlieward.lousynetvanishplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LousyNetVanishPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
