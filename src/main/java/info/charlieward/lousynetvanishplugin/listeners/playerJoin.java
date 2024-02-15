0f9e8a14482fc762563fa47561f87202b0889e5e
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-15 16:56:46 +0000

    spoke too soon

--- src/main/java/info/charlieward/lousynetvanishplugin/listeners/playerJoin.java
+++ src/main/java/info/charlieward/lousynetvanishplugin/listeners/playerJoin.java
@@ -3,6 +3,7 @@ package info.charlieward.lousynetvanishplugin.listeners;
 import info.charlieward.lousynetvanishplugin.LousyNetVanishPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,16 +26,16 @@ public class playerJoin implements Listener {
             String playerName = player.getDisplayName();
             String vanishedPlayers = plugin.jedis.get("VanishPlayers");
             String[] vanishPlayersIndividual = vanishedPlayers.split(",");
-
             for (int i = 0; i < vanishPlayersIndividual.length; i++) {
                 if (Objects.equals(playerName, vanishPlayersIndividual[i])) {
                     for (Player people : Bukkit.getOnlinePlayers()){
                         if(!people.hasPermission("LousyNetVanish.Vanish")){
                             people.hidePlayer(plugin, player);
-                            event.setJoinMessage(null);
-                            player.sendMessage(ChatColor.BLUE + "[LousyNet-VanishPlugin] " + ChatColor.WHITE + "You are currently vanished");
                         }
                     }
+                    event.setJoinMessage(null);
+                    player.sendMessage(ChatColor.BLUE + "[LousyNet-VanishPlugin] " + ChatColor.WHITE + "You are currently vanished");
+                    player.setGameMode(GameMode.SPECTATOR);
                 }
             }
         }
